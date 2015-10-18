b0e25d122b079d3cdc7be40f7d55911a5136ad62
Carlos <carlosrive@bitbucket.org>
2015-10-18 16:23:36 +0200

    Es mostra un selector de llocs / Es busca la localització més propera a la situació actual

--- app/src/main/java/jmcdw/bcnsobrerodes/PlacePickerActivity.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/PlacePickerActivity.java
@@ -5,25 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.maps.android.SphericalUtil;
 
 
-public class PlacePickerActivity extends AppCompatActivity {
+public class PlacePickerActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String LOG_TAG = "PlacesAPIActivity";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
     private static final int PLACE_PICKER_REQUEST = 1;
     private TextView mName;
     private TextView mAddress;
     private TextView mAttributions;
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
+    private TextView mLikeliPlace;
+    private LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(41.342535, 2.149677), new LatLng(41.745079, 2.167947));
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +49,11 @@ public class PlacePickerActivity extends AppCompatActivity {
         mName = (TextView) findViewById(R.id.textView);
         mAddress = (TextView) findViewById(R.id.textView2);
         mAttributions = (TextView) findViewById(R.id.textView3);
+        mLikeliPlace = (TextView) findViewById(R.id.textView4);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .build();
         Button pickerButton = (Button) findViewById(R.id.pickerButton);
         pickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +61,7 @@ public class PlacePickerActivity extends AppCompatActivity {
                 try {
                     PlacePicker.IntentBuilder intentBuilder =
                             new PlacePicker.IntentBuilder();
-                    intentBuilder.setLatLngBounds(BOUNDS_MOUNTAIN_VIEW);
+                    intentBuilder.setLatLngBounds(BOUNDS);
                     Intent intent = intentBuilder.build(getApplicationContext());
                     startActivityForResult(intent, PLACE_PICKER_REQUEST);
 
@@ -50,6 +72,46 @@ public class PlacePickerActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button whereIam = (Button) findViewById(R.id.whereButton);
+        whereIam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mGoogleApiClient.isConnected()) {
+                    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                            .getCurrentPlace(mGoogleApiClient, null);
+                    result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+                        @Override
+                        public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                            /*for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                                Log.i(LOG_TAG, String.format("Place '%s' with " +
+                                                "likelihood: %g",
+                                        placeLikelihood.getPlace().getName(),
+                                        placeLikelihood.getLikelihood()));
+                            }*/
+
+                            PlaceLikelihood placeLikelihood = likelyPlaces.get(0);
+                            String content = "";
+                            if (placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty(placeLikelihood.getPlace().getName()))
+                                content = "Most likely place: " + placeLikelihood.getPlace().getName() + "\n";
+                            if (placeLikelihood != null)
+                                content += "Percent change of being there: " + (int) (placeLikelihood.getLikelihood() * 100) + "%";
+                            mLikeliPlace.setText(content);
+                            BOUNDS =  convertCenterAndRadiusToBounds(placeLikelihood.getPlace().getLatLng(),50);
+
+                            likelyPlaces.release();
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    public LatLngBounds convertCenterAndRadiusToBounds(LatLng center, double radius) {
+        LatLng southwest = SphericalUtil.computeOffset(center, radius * Math.sqrt(2.0), 225);
+        LatLng northeast = SphericalUtil.computeOffset(center, radius * Math.sqrt(2.0), 45);
+        return new LatLngBounds(southwest, northeast);
     }
 
     @Override
@@ -76,4 +138,13 @@ public class PlacePickerActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(this,
+                "Google Places API connection failed with error code:" +
+                        connectionResult.getErrorCode(), Toast.LENGTH_LONG).show();
+    }
 }
