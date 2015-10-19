140870ebbef517e3552387e97e67679c5db16686
Carlos <carlosrive@bitbucket.org>
2015-10-19 16:22:04 +0200

    Mostrar ubicació actual real // posar les coses mes maques

--- app/src/main/java/jmcdw/bcnsobrerodes/PlacePickerActivity.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/PlacePickerActivity.java
@@ -2,10 +2,10 @@ package jmcdw.bcnsobrerodes;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +16,8 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
@@ -34,10 +31,11 @@ public class PlacePickerActivity extends AppCompatActivity implements
     private static final String LOG_TAG = "PlacesAPIActivity";
     private static final int GOOGLE_API_CLIENT_ID = 0;
     private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int RADIUS_LOCATION = 50;
     private TextView mName;
     private TextView mAddress;
     private TextView mAttributions;
-    private TextView mLikeliPlace;
+    private TextView mLocation;
     private LatLngBounds BOUNDS = new LatLngBounds(
             new LatLng(41.342535, 2.149677), new LatLng(41.745079, 2.167947));
     private GoogleApiClient mGoogleApiClient;
@@ -46,21 +44,18 @@ public class PlacePickerActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_picker);
-        mName = (TextView) findViewById(R.id.textView);
-        mAddress = (TextView) findViewById(R.id.textView2);
-        mAttributions = (TextView) findViewById(R.id.textView3);
-        mLikeliPlace = (TextView) findViewById(R.id.textView4);
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
-                .build();
+
+        buildTexts();
+        buildGoogleAPIClient();
+
         Button pickerButton = (Button) findViewById(R.id.pickerButton);
         pickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mGoogleApiClient.isConnected())
+                    BOUNDS = convertCenterAndRadiusToBounds(whereIam(), RADIUS_LOCATION);
                 try {
-                    PlacePicker.IntentBuilder intentBuilder =
-                            new PlacePicker.IntentBuilder();
+                    PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
                     intentBuilder.setLatLngBounds(BOUNDS);
                     Intent intent = intentBuilder.build(getApplicationContext());
                     startActivityForResult(intent, PLACE_PICKER_REQUEST);
@@ -73,54 +68,51 @@ public class PlacePickerActivity extends AppCompatActivity implements
             }
         });
 
-        Button whereIam = (Button) findViewById(R.id.whereButton);
+        //NOMES PER ENSENYAR LA FUNCIONALITAT
+        final Button whereIam = (Button) findViewById(R.id.whereButton);
         whereIam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mGoogleApiClient.isConnected()) {
-                    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
-                            .getCurrentPlace(mGoogleApiClient, null);
-                    result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
-                        @Override
-                        public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
-                            /*for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                                Log.i(LOG_TAG, String.format("Place '%s' with " +
-                                                "likelihood: %g",
-                                        placeLikelihood.getPlace().getName(),
-                                        placeLikelihood.getLikelihood()));
-                            }*/
-
-                            PlaceLikelihood placeLikelihood = likelyPlaces.get(0);
-                            String content = "";
-                            if (placeLikelihood != null && placeLikelihood.getPlace() != null && !TextUtils.isEmpty(placeLikelihood.getPlace().getName()))
-                                content = "Most likely place: " + placeLikelihood.getPlace().getName() + "\n";
-                            if (placeLikelihood != null)
-                                content += "Percent change of being there: " + (int) (placeLikelihood.getLikelihood() * 100) + "%";
-                            mLikeliPlace.setText(content);
-                            BOUNDS =  convertCenterAndRadiusToBounds(placeLikelihood.getPlace().getLatLng(),50);
-
-                            likelyPlaces.release();
-                        }
-                    });
-                }
+                whereIam();
             }
         });
 
     }
 
-    public LatLngBounds convertCenterAndRadiusToBounds(LatLng center, double radius) {
+    private LatLng whereIam() {
+        Location currentLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+        double longitude = currentLocation.getLongitude();
+        double latitude = currentLocation.getLatitude();
+        LatLng location = new LatLng(latitude, longitude);
+        mLocation.setText("Current location \n long: " + longitude + " / lat: " + latitude);
+        return location;
+    }
+
+    private void buildTexts() {
+        mName = (TextView) findViewById(R.id.textView);
+        mAddress = (TextView) findViewById(R.id.textView2);
+        mAttributions = (TextView) findViewById(R.id.textView3);
+        mLocation = (TextView) findViewById(R.id.textView4);
+    }
+
+    private void buildGoogleAPIClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .build();
+    }
+
+    private LatLngBounds convertCenterAndRadiusToBounds(LatLng center, double radius) {
         LatLng southwest = SphericalUtil.computeOffset(center, radius * Math.sqrt(2.0), 225);
         LatLng northeast = SphericalUtil.computeOffset(center, radius * Math.sqrt(2.0), 45);
         return new LatLngBounds(southwest, northeast);
     }
 
     @Override
-    protected void onActivityResult(int requestCode,
-                                    int resultCode, Intent data) {
-
-        if (requestCode == PLACE_PICKER_REQUEST
-                && resultCode == Activity.RESULT_OK) {
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
             final Place place = PlacePicker.getPlace(data, this);
             final CharSequence name = place.getName();
             final CharSequence address = place.getAddress();
