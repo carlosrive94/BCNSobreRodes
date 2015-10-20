889c1904d1030a7454a3237f24da612f38636c9e
Mike <miguel.angel.aula@est.fib.upc.edu>
2015-10-20 21:46:47 +0200

    El calcul de rutes funciona (ho he testejat a part) pero hi ha un error a l'hora de passar els parametres de "to" i "from" de FromToActivity a MapPane per tant ara mateix no es pot veure fins que es solucioni

--- app/src/main/java/jmcdw/bcnsobrerodes/FromToActivity.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/FromToActivity.java
@@ -1,12 +1,23 @@
 package jmcdw.bcnsobrerodes;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FromToActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from_to);
+    }
+
+    public void onSearchRoute(View view) {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("from", R.id.FromText);
+        returnIntent.putExtra("to", R.id.ToText);
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 }
--- app/src/main/java/jmcdw/bcnsobrerodes/MapPane.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/MapPane.java
@@ -1,9 +1,12 @@
 package jmcdw.bcnsobrerodes;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,8 +16,18 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 //import android.appwidget.;
@@ -30,6 +43,42 @@ public class MapPane extends Activity implements OnMapReadyCallback {
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (data.hasExtra("from") && data.hasExtra("to")) {
+                //dibuixa la ruta des de "from" fins a "to"
+                List<Address> addressList = null;
+                Geocoder geocoder = new Geocoder(this);
+                try {
+                    //afegim ", Barcelona" al final del string per obtenir l'adreça
+                    addressList = geocoder.getFromLocationName(getIntent().getStringExtra("from") + ", Barcelona", 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address from = addressList.get(0);
+                try {
+                    //afegim ", Barcelona" al final del string per obtenir l'adreça
+                    addressList = geocoder.getFromLocationName(getIntent().getStringExtra("to") + ", Barcelona", 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Address to = addressList.get(0);
+                LatLng origin = new LatLng(from.getLatitude(),from.getLongitude());
+                LatLng dest = new LatLng(to.getLatitude(),to.getLongitude());
+                myMap.addMarker(new MarkerOptions().position(origin).title(from.getAddressLine(0)));
+                myMap.addMarker(new MarkerOptions().position(dest).title(to.getAddressLine(0)));
+                // Getting URL to the Google Directions API
+                String url = getDirectionsUrl(origin, dest);
+
+                DownloadTask downloadTask = new DownloadTask();
+
+                // Start downloading json data from Google Directions API
+                downloadTask.execute(url);
+            }
+        }
     }
 
     public void onSearch(View view) {
@@ -51,11 +100,12 @@ public class MapPane extends Activity implements OnMapReadyCallback {
             myMap.addMarker(new MarkerOptions().position(latLng).title(address.getAddressLine(0)));
             myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
         }
+
     }
 
     public void onClickRoute(View view) {
         Intent i = new Intent(getApplicationContext(),FromToActivity.class);
-        startActivity(i);
+        startActivityForResult(i,1);
     }
 
     @Override
@@ -66,4 +116,169 @@ public class MapPane extends Activity implements OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(barcelona, 13));
         myMap = map;
     }
+
+    /*
+
+    Funcions i classes de rutes:
+
+    */
+
+    private String getDirectionsUrl(LatLng origin,LatLng dest){
+
+        // Origin of route
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+
+        // Building the parameters to the web service
+        String parameters = str_origin+"&"+str_dest+"&"+sensor;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+
+        return url;
+    }
+    /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException{
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Error downloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    // Fetches data from url passed
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try{
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+        // Executes in UI thread, after the execution of
+        // doInBackground()
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+        }
+    }
+
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            // Traversing through all the routes
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(2);
+                lineOptions.color(Color.RED);
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            myMap.addPolyline(lineOptions);
+        }
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
 }
