866c642dd30e80a0ad342b7dbdadf4ac616e59d7
Daniel Villanueva <daniel.villanueva.sanchez@est.fib.upc.edu>
2015-11-11 11:00:36 +0100

    Continuant amb login

--- app/src/main/java/jmcdw/bcnsobrerodes/LoginActivity.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/LoginActivity.java
@@ -117,7 +117,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
                         Toast t;
 
-                        if (usuario.equals("") || password.equals("") || verif.equals(null) || mail.equals(null)) {
+                        if (usuario.equals("") || password.equals("") || verif.equals("") || mail.equals("")) {
                             t = Toast.makeText(LoginActivity.this, "Please fill all the blankets", Toast.LENGTH_SHORT);
                             t.show();
                         } else if (!password.equals(verif)) {
@@ -125,14 +125,28 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             t.show();
                         }
 
-                        try{
-                            addUser(usuario, password, mail);
-                            t = Toast.makeText(LoginActivity.this, "User " + usuario +" created successfully", Toast.LENGTH_SHORT);
-                            t.show();
-                        }
+                        else try {
 
-                        catch(Exception exception){
-                            t = Toast.makeText(LoginActivity.this, exception.getMessage(),Toast.LENGTH_SHORT);
+                            int success = checkUser(usuario, password, mail);
+                            if (success == 0) addUser(usuario, password, mail);
+
+                            switch (success) {
+                                case 0:
+                                    t = Toast.makeText(LoginActivity.this, "User " + usuario + " created", Toast.LENGTH_SHORT);
+                                    t.show();
+                                    break;
+                                case -1:
+                                    t = Toast.makeText(LoginActivity.this, "User " + usuario + " already exists", Toast.LENGTH_SHORT);
+                                    t.show();
+                                    break;
+                                case -2:
+                                    t = Toast.makeText(LoginActivity.this, "There is already an account with this email", Toast.LENGTH_SHORT);
+                                    t.show(); break;
+
+                            }
+
+                        } catch (Exception exception) {
+                            t = Toast.makeText(LoginActivity.this, exception.getMessage(), Toast.LENGTH_SHORT);
                             t.show();
                         }
 
@@ -147,12 +161,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         builder.create().show();
     }
 
+    public int checkUser(String user, String password, String email) throws Exception{
+        Persistence persistence = new Persistence(this);
+        String q1 = "select username, email from users where username=\"" + user + "\" or email=\"" + email +"\"";
+        String row[] = persistence.execute(q1, "select").get().split("-");
+        //persistence.cancel(false);
+
+        if (row[0].equals(user)) return -1;
+        else if (row[1].equals(email)) return -2;
+        else return 0;
+    }
+
     public void addUser(String user, String password, String email) throws Exception{
         String query = "insert into users(username, email,password)" +
                 " values(\"" + user + "\", \"" + email + "\", \"" + password + "\")";
         Persistence persistence = new Persistence(this);
         persistence.execute(query, "modification");
     }
+
 
     public int userVerification(String user, String password) throws ExecutionException, InterruptedException{
         String query = "select admin,password from users where username=\"" + user + "\"";
