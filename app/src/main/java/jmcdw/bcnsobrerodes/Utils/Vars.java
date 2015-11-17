081850f7a58f6a51200806781469773f920df151
Carlos Rivero <carlos.rivero@est.fib.upc.edu>
2015-11-17 13:43:43 +0100

    estacions no accessibles

---
+++ app/src/main/java/jmcdw/bcnsobrerodes/Utils/Vars.java
@@ -0,0 +1,21 @@
+package jmcdw.bcnsobrerodes.Utils;
+
+public class Vars {
+
+    //Estacions no accessibles
+    private String ESTACIO_PLACASANTS = "ChIJ9UOQWn2YpBIRRZa04WZC96k";
+    private String ESTACIO_ESPANYA = "ChIJt6GYbHmipBIRDPoEUXepx9A";
+    private String ESTACIO_URQUINAONA = "ChIJD3hvRfCipBIR8_l9xAyoEWQ";
+    private String ESTACIO_CLOT = "ChIJB_UlKyajpBIRE4hDxp28i_Q";
+    private String ESTACIO_ZONA_UNIVERSITARIA = "ChIJudibfFaYpBIRZ5__zLmaPgY";
+    private String ESTACIO_VALLCARCA = "ChIJ4xlZkAeYpBIRiY6JFee11YA";
+    private String ESTACIO_MARAGALL = "ChIJpQdg-Cy9pBIRv5Ip3E_PHuM";
+    private String ESTACIO_VERDAGUER = "ChIJO4AocuqipBIRbNhvaZIDl2Y";
+    private String ESTACIO_JAUME_I = "ChIJB6daOfmipBIRtw_PKDGQ1DU";
+    private String ESTACIO_CIUTADELLA_VILA_OLIMPICA = "ChIJve5SlQWjpBIRe0OU8fwjUBg";
+    private String ESTACIO_POBLENOU = "ChIJPVegaD-jpBIRDZ2cFb5NR6E";
+    private String ESTACIO_VIRREI_AMAT = "ChIJO4nk5i-9pBIRPSysH2uTxFo";
+    private String ESTACIO_COLLBLANC = "ChIJH4RcP_GYpBIRFPA5wkbVkJU";
+
+
+}
