658aea9b42c72f840bb0cb23e2eea96ed1f8067c
WANG YANG YE <wangyang.ye@est.fib.upc.edu>
2015-11-24 13:57:20 +0100

    approve obstacles

--- app/src/main/java/jmcdw/bcnsobrerodes/MySimpleAdapter.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/MySimpleAdapter.java
@@ -2,6 +2,7 @@ package jmcdw.bcnsobrerodes;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 import java.util.List;
 import java.util.Map;
 
+import jmcdw.bcnsobrerodes.Utils.Persistence;
+
 /**
  * Created by ywy on 2015/11/21.
  */
@@ -24,6 +27,9 @@ public class MySimpleAdapter extends BaseAdapter {
     private int layoutID;
     private String flag[];
     private int ItemIDs[];
+    private int auxposition;
+    private Context context;
+
     public MySimpleAdapter(Context context, List<Map<String, Object>> list,
                            int layoutID, String flag[], int ItemIDs[]) {
 
@@ -32,6 +38,8 @@ public class MySimpleAdapter extends BaseAdapter {
         this.layoutID = layoutID;
         this.flag = flag;
         this.ItemIDs = ItemIDs;
+        this.auxposition = 0;
+        this.context = context;
     }
     @Override
     public int getCount() {
@@ -49,8 +57,8 @@ public class MySimpleAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        auxposition = position;
         convertView = mInflater.inflate(layoutID, null);
         for (int i = 0; i < flag.length; i++) {
             if (convertView.findViewById(ItemIDs[i]) instanceof TextView) {
@@ -65,11 +73,19 @@ public class MySimpleAdapter extends BaseAdapter {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        String username = (String) list.get(position).get(flag[0]);
+                        String[] latlong =  ((String) list.get(position).get(flag[1])).split(",");
+                        double lat = Double.parseDouble(latlong[0]);
+                        double lng = Double.parseDouble(latlong[1]);
 
-                        new AlertDialog.Builder(Verify.ma)
-                                .setTitle("DIY SimpleAdapter")
-                                .setMessage("Approve Success！")
-                                .show();
+                        String query = "update Obstacles " +
+                                "set verificat = 1 " +
+                                "where afegit_per =\""+username+"\" AND longitud =\""+lng+"\" AND latitud =\""+lat+"\"";
+
+                        Persistence persistence = new Persistence(context);
+                        persistence.execute(query, "modification");
+                        Intent intent = new Intent(context, Verify.class);
+                        context.startActivity(intent);
                     }
                 });
         ((Button)convertView.findViewById(R.id.Denybtn)).setOnClickListener(
