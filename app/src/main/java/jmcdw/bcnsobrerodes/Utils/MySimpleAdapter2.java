18d8181046b7b198bd27eca1645993a2341c01de
WANG YANG YE <wangyang.ye@est.fib.upc.edu>
2016-01-14 18:39:56 +0100

    mostrar place al detail

--- app/src/main/java/jmcdw/bcnsobrerodes/Utils/MySimpleAdapter2.java
+++ app/src/main/java/jmcdw/bcnsobrerodes/Utils/MySimpleAdapter2.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
+import android.location.Geocoder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +30,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.PolyUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import jmcdw.bcnsobrerodes.Delete;
@@ -118,8 +123,20 @@ public class MySimpleAdapter2 extends BaseAdapter {
                         double lat = Double.parseDouble(latlong[0]);
                         double lng = Double.parseDouble(latlong[1]);
 
+                        LatLng loc = new LatLng(lat, lng);
+                        Geocoder geocoder;
+                        List<Address> addresses = new ArrayList<>();
+                        geocoder = new Geocoder(context, Locale.getDefault());
+                        try {
+                            addresses = geocoder.getFromLocation(loc.latitude, loc.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Address addr = addresses.get(0);
+
                         Toast.makeText(context, "\nAdded by: " + username
-                                        + "\nDescription: " + (String) list.get(position).get(flag[1]) +"\nLatitud: " + lat + "\nLongitud: " + lng,
+                                        + "\nDescription: " + (String) list.get(position).get(flag[1]) +"\nLatitud: " + lat + "\nLongitud: " + lng
+                                        + "\nPlace: " + addr.getAddressLine(0),
                                 Toast.LENGTH_LONG).show();
                     }
                 });
